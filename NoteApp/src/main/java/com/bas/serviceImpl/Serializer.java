1684a33735210ea24ed6ea0a3a30e2a542239408
Vladimir <LesnickBear@gmail.com>
2018-04-18 12:29:55 +0300

    Saving and loading from jar hardcoded and fixed

--- NoteApp/src/main/java/com/bas/serviceImpl/Serializer.java
+++ NoteApp/src/main/java/com/bas/serviceImpl/Serializer.java
@@ -4,21 +4,18 @@ import com.bas.model.INote;
 import com.bas.service.ISerializer;
 
 import java.io.*;
-import java.net.URL;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 
 public class Serializer implements ISerializer {
-    private static final String FILE_PATH = "Storage.dat";
+    private static final String FILE_PATH = "D:\\Storage.dat";
 
     @Override
     public List<INote> get() {
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(Objects.requireNonNull(classLoader.getResource(FILE_PATH)).getFile());
-        try (ObjectInputStream out = new ObjectInputStream(new FileInputStream(file ))) {
+        try ( InputStream in = new FileInputStream(new File(FILE_PATH))){
+            ObjectInputStream out = new ObjectInputStream(in);
             return (LinkedList<INote>) out.readObject();
-
         } catch (Exception e) {
             return new LinkedList<>();
         }
@@ -27,8 +24,12 @@ public class Serializer implements ISerializer {
 
     @Override
     public Boolean save(List<INote> notesList) {
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(Objects.requireNonNull(classLoader.getResource(FILE_PATH)).getFile());
+        File file = new File(FILE_PATH);
+        try {
+            file.createNewFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file))) {
             out.writeObject(notesList);
         } catch (Exception e) {
